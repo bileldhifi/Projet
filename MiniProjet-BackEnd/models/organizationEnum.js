cc5d10551b97fd2ceb334159eea7617e0fb19821
Oumayma <oumayma.cherif@esprit.tn>
2023-11-19 22:20:13 +0100

    OrganizationEnum

---
+++ MiniProjet-BackEnd/models/organizationEnum.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const OrganizationEnum = ['Tunivisions', 'Rotaract', 'Croissant Rouge']; // Initial predefined values
+
+module.exports = OrganizationEnum;
--- MiniProjet-BackEnd/server.js
+++ MiniProjet-BackEnd/server.js
@@ -1,63 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require("morgan")
+const morgan = require('morgan');
 const path = require('path');
-const swaggerDocument = require("./swagger.json")
-const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
 
+const eventsRoute = require('./routes/events');
+//const organizationRoutes = require('./routes/organization'); // Adjust the path accordingly
 
-
-const eventsRoute = require('./routes/events')
-
-const organizationRoute = require('./routes/organization'); // Import the organization route
-
-
-var cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const app = express()
-const hostname = "127.0.0.1"
-const port = 3000
+const app = express();
+const port = 3000;
 
-// app.use(cors())
-//app.use(express.json())
-app.use(morgan('dev'))
-
-app.use(express.urlencoded({ extended: true }))
-//DB
-mongoose.set('debug', true)
-mongoose.Promise = global.Promise;
-mongoose
-    .connect('mongodb://127.0.0.1:27017/Eco')
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => {
-        console.log(err);
-    })
-
-
-
-
-
+// Body parsers
 app.use(express.urlencoded({ extended: true, limit: '400mb' }));
-
 app.use(express.json({
   verify: (req, res, buf) => {
-    req.rawBody = buf.toString()
+    req.rawBody = buf.toString();
   },
   limit: '400mb'
 }));
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(bodyParser.json())
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));////////
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// MongoDB connection
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/Eco')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
+// Middleware (morgan and cors)
+app.use(morgan('dev'));
+app.use(cors());
+
+// Routes
+app.use('/events', eventsRoute);
+//app.use('/organizations', organizationRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port`, port);
+  console.log(`Server is running on port ${port}`);
 });
-
-app.use('/events', eventsRoute)//swagger done
-app.use('/organizations', organizationRoute);
