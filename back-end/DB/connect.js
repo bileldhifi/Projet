aae62acbbf18cfd652b0ce82892243564f279e2c
bileldhifi <bileldhifi37@gmail.com>
2023-11-22 00:26:45 +0100

    deleted db

--- back-end/DB/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose
-  .connect(
-    "mongodb+srv://bilel:bilel@cluster0.xspsj0f.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("connected to DB"))
-  .catch((error) => console.log(error));
-
-module.exports = mongoose;
